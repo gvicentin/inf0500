661e32a619a835f9979bc06934790c72f33b8241
Guilherme Vicentin <gvicentin@ciandt.com>
2024-02-15 13:31:34 -0300

    Ouput examples

--- inf0540/tasks/task02.py
+++ inf0540/tasks/task02.py
@@ -3,12 +3,29 @@
 # Usage: task02.py TARGET_IP
 #
 # Executando para os seguntions hosts:
-# task02.py 143.106.7.31
-# task02.py 143.106.16.156
-# task02.py 172.16.10.130
-# task02.py 143.106.16.164
-# task02.py 143.106.23.145
-# task02.py 143.106.23.158
+#
+# $ task02.py 143.106.7.31
+# TTL: 63
+# Sistema: Linux, numero de saltos: 1
+#
+# $ task02.py 143.106.16.156
+# TTL: 64
+# Sistema: Linux, numero de saltos: 0
+#
+# $ task02.py 172.16.10.130
+# TTL: 254
+# Sistema: Equipment, numero de saltos: 1
+#
+# $ task02.py 143.106.16.164
+# TTL: 128
+# Sistema: Microsoft, numero de saltos: 0
+# 
+# $ task02.py 143.106.23.145
+# TTL: 127
+# Sistema: Microsoft, numero de saltos: 1
+#
+# $ task02.py 143.106.23.158
+# Falha ao executar: ping 143.106.23.158
 import re
 import sys
 import subprocess
